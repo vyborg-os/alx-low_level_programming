902d89638070a86fe0f75e27960253ed24c36233
vyborg <41639842+vyborg-os@users.noreply.github.com>
2023-07-13 12:15:10 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* print a line with put function */
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
