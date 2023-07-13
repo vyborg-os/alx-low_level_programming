eb9542f466df3bd7b1bb1bc978517d386bf36c20
vyborg <41639842+vyborg-os@users.noreply.github.com>
2023-07-13 12:13:55 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* print a line with put function */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
